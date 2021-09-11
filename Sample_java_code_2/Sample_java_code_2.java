7f0a8562afaf583735ba26adf294f37472f69330
Kaustav <70485937+Kaustav86x@users.noreply.github.com>
2021-09-12 02:21:16 +0530

    Java Source Code File

---
+++ Sample_java_code_2/Sample_java_code_2.java
@@ -0,0 +1,13 @@
+// Simple java code using method 
+public class Sample_java_code_2
+{
+   static void alpha_add(int n) 
+   {
+     System.out.println("This is " + n);
+     
+   }
+   public static void main(String args[]) 
+   {
+      alpha_add(10);
+   }
+}
