300ebfb2b8859f0103346670313cdc8f1411b70a
Kaustav <70485937+Kaustav86x@users.noreply.github.com>
2021-10-26 00:34:51 +0530

    Add files via upload

---
+++ Topic Wise Java Code/Runtime Polymorphism& Inheritance/RunTimePoly2.java
@@ -0,0 +1,46 @@
+class Asia
+{
+    int a;
+    Asia()
+    {
+        System.out.println("\nThis is the biggest continent - Asia.");
+    }
+    Asia(int c)
+    {
+        a = c;
+        System.out.println("\nThere are total "+a+" countries in Asia.");
+    }
+    public void Us(String coun)
+    {
+        System.out.println("\nThe country we live in is "+coun+".");
+    }
+}
+class India extends Asia
+{
+    India()
+    {
+        System.out.println("\nIndia is the country we live in.");
+    }
+    India(int n)
+    {
+        // constructor inherited
+        super(n);
+        System.out.println("\nIndia is the "+n+"th largest country in the world.");  
+    }
+    // method overriding
+    public void Us(String state)
+    {
+        // method inherited using super()
+        super.Us("India");
+        System.out.println("\nThe largest state of India is - "+state);
+    }
+}
+public class RunTimePoly2 {
+    public static void main(String args[])
+    {
+        // runtime polymorphism
+        Asia obj1 = new India();
+        Asia obj2 = new India(7);
+        obj1.Us("Rajasthan");
+    }
+}
