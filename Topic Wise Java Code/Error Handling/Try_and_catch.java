e8681ce080650e9415bcabcc98c9697771d4d8d1
Kaustav <70485937+Kaustav86x@users.noreply.github.com>
2021-11-05 00:51:02 +0530

    Update Try_and_catch.java

--- Topic Wise Java Code/Error Handling/Try_and_catch.java
+++ Topic Wise Java Code/Error Handling/Try_and_catch.java
@@ -1,1 +1,16 @@
+public class Try_and_catch {
+    public static void main(String[] args) {
+        int d, a;
 
+        try 
+        {
+            d = 0;
+            a = 42 / d; // exception
+            System.out.println("This line will not get executed");
+        }
+        catch (ArithmeticException excp) // named a particular exception
+        {
+            System.out.println("Divison by zero didvison has occured. Stopped the execution furthur");
+        }
+    }
+}
