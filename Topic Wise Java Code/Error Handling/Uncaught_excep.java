30bae1dfdc3f5cb2c398ab5ff8955b26d33ffa9a
Kaustav <70485937+Kaustav86x@users.noreply.github.com>
2021-11-05 00:44:52 +0530

    Update Uncaught_excep.java

--- Topic Wise Java Code/Error Handling/Uncaught_excep.java
+++ Topic Wise Java Code/Error Handling/Uncaught_excep.java
@@ -1,1 +1,13 @@
-
+public class Uncaught_exception 
+{
+    static void sub() // without creating object
+    {
+        int d = 0;
+        int a = 50 / d; // exception
+        System.out.println(a);
+    }
+    public static void main(String args[])
+    {
+        Error1.sub(); // without creating object
+    }
+}
