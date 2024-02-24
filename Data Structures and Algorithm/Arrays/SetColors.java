dc65cde25c4cee4452bf2b4a5cb206cad453be70
Kaustav <70485937+Kaustav86x@users.noreply.github.com>
2024-02-24 14:26:11 +0530

    24-02-24
    Input: nums = [2,0,2,1,1,0]
    Output: [0,0,1,1,2,2]

---
+++ Data Structures and Algorithm/Arrays/SetColors.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void sortColors(int[] nums) {
+    // optimal solution (Dutch National Flag algorithm) (0 ms)
+    int low = 0; 
+    int mid = 0; 
+    int high = nums.length-1;
+    int temp;
+    while(mid <= high)
+    {
+        if(nums[mid] == 0)
+        {
+            temp = nums[mid];
+            nums[mid] = nums[low];
+            nums[low] = temp;
+            mid ++;
+            low ++;
+        }
+        else if(nums[mid] == 1)
+        {
+            mid ++;
+        }
+        else if(nums[mid] == 2)
+        {
+            // swap(nums[mid], nums[high]);
+            temp = nums[mid];
+            nums[mid] = nums[high];
+            nums[high] = temp;
+            high --;
+        }
+    }
+    
+    }
+}
